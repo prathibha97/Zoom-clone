f656cfe9ef402c7763faafd4b0ca3495b3f77f35
Prathibha <prsthibha@gmail.com>
2020-12-13 12:20:51 +0530

    socket.io added

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
@@ -11,8 +13,13 @@ navigator.mediaDevices
   })
   .then((stream) => {
     myVideoStream = stream;
-    addVideoStream(myVideo, stream)
+    addVideoStream(myVideo, stream);
   });
+
+socket.emit('join-room', ROOM_ID);
+socket.on('user-connected', ()=>{
+    connectToNewUser()
+})
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
@@ -21,3 +28,7 @@ const addVideoStream = (video, stream) => {
   });
   videoGrid.append(video);
 };
+
+const connectToNewUser = ()=>{
+    console.log('new user');
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const { v4: uudiv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
@@ -10,8 +11,15 @@ app.get('/', (req, res) => {
   res.redirect(`/:${uudiv4()}`);
 });
 
-app.get('/:room', (req,res)=>{
-    res.render('room', {roomId:req.params.room})
-})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected');
+  });
+});
 
 server.listen(3000, () => console.log('server listening on port 3000'));
