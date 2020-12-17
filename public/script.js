f12e7fb8875bc14bf21c740266f87bbdeeba755f
Prathibha <prsthibha@gmail.com>
2020-12-17 18:28:34 +0530

    audio/video play stop functionality added

--- public/script.js
+++ public/script.js
@@ -74,14 +74,57 @@ const scrollToBottom = () => {
   d.scrollTop(d.prop('scrollHeight'));
 };
 
-// mute video
+// mute audio
 const muteUnmute = () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
     myVideoStream.getAudioTracks()[0].enabled = false;
     setUnmuteButton();
   } else {
-    setUnmuteButton();
+    setMuteButton();
     myVideoStream.getAudioTracks()[0].enabled = true;
   }
+};
+
+const setMuteButton = () => {
+  const html = `
+  <i class="fas sa-microphone"></i>
+  <span>Mute</span>
+  `;
+  document.querySelector('.main__mute_button').innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+  <i class="unmute fas fa-microphone-slash"></i>
+  <span>Unmute</span>
+  `;
+  document.querySelector('.main__mute_button').innerHTML = html;
+};
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setStopVideo = () => {
+  const html = `
+  <i class="fas fa-video-slash stop"></i>
+  <span>Play Video</span>
+  `;
+  document.querySelector('.main__video_button').innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="fas fa-video"></i>
+  <span>Stop Video</span>
+  `;
+  document.querySelector('.main__video_button').innerHTML = html;
 };
